95cbad97a435effab9718253c298d96c6214d849
nelson <>
2022-07-01 17:53:14 +0800

    add console logging to test login

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,201 +1,159 @@
-import Head from 'next/head';
-import NextLink from 'next/link';
-import {useRouter} from 'next/router';
-import {useFormik} from 'formik';
-import * as Yup from 'yup';
-import {Box, Button, Container, Grid, Link, TextField, Typography} from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import {Facebook as FacebookIcon} from '../icons/facebook';
-import {Google as GoogleIcon} from '../icons/google';
+import Head from "next/head";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Box, Button, Container, Grid, Link, TextField, Typography } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Facebook as FacebookIcon } from "../icons/facebook";
+import { Google as GoogleIcon } from "../icons/google";
 import fetchJson from "../lib/fetchJson";
 
-
 const Login = () => {
-    const router = useRouter();
-    const formik = useFormik({
-        initialValues: {
-            username: 'broker123',
-            password: 'Password123'
-        },
-        validationSchema: Yup.object({
-            username: Yup
-                .string()
-                .max(255)
-                .required(
-                    'Username is required'),
-            password: Yup
-                .string()
-                .max(255)
-                .required(
-                    'Password is required')
-        }),
-        onSubmit: values => {
-            fetchJson("http://45.12.144.105:8080/api/login", {
-                method: 'POST',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(values)
-            });
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      username: "broker123",
+      password: "Password123",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().max(255).required("Username is required"),
+      password: Yup.string().max(255).required("Password is required"),
+    }),
+    onSubmit: async (values) => {
+      console.log("sending POST request");
+      const res = await fetchJson("http://45.12.144.105:8080/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(values),
+      });
+      console.log(`response: ${JSON.stringify(res)}`);
+    },
+  });
 
-        }
-    });
-
-    return (
-        <>
-            <Head>
-                <title>Login | Broker Demo</title>
-            </Head>
-            <Box
-                component="main"
-                sx={{
-                    alignItems: 'center',
-                    display: 'flex',
-                    flexGrow: 1,
-                    minHeight: '100%'
-                }}
-            >
-                <Container maxWidth="sm">
-                    <NextLink
-                        href="/"
-                        passHref
-                    >
-                        <Button
-                            component="a"
-                            startIcon={<ArrowBackIcon fontSize="small"/>}
-                        >
-                            Dashboard
-                        </Button>
-                    </NextLink>
-                    <form onSubmit={formik.handleSubmit}>
-                        <Box sx={{my: 3}}>
-                            <Typography
-                                color="textPrimary"
-                                variant="h4"
-                            >
-                                Sign in
-                            </Typography>
-                            <Typography
-                                color="textSecondary"
-                                gutterBottom
-                                variant="body2"
-                            >
-                                Sign in on the internal platform
-                            </Typography>
-                        </Box>
-                        <Grid
-                            container
-                            spacing={3}
-                        >
-                            <Grid
-                                item
-                                xs={12}
-                                md={6}
-                            >
-                                <Button
-                                    color="info"
-                                    fullWidth
-                                    startIcon={<FacebookIcon/>}
-                                    onClick={formik.handleSubmit}
-                                    size="large"
-                                    variant="contained"
-                                >
-                                    Login with Facebook
-                                </Button>
-                            </Grid>
-                            <Grid
-                                item
-                                xs={12}
-                                md={6}
-                            >
-                                <Button
-                                    fullWidth
-                                    color="error"
-                                    startIcon={<GoogleIcon/>}
-                                    onClick={formik.handleSubmit}
-                                    size="large"
-                                    variant="contained"
-                                >
-                                    Login with Google
-                                </Button>
-                            </Grid>
-                        </Grid>
-                        <Box
-                            sx={{
-                                pb: 1,
-                                pt: 3
-                            }}
-                        >
-                            <Typography
-                                align="center"
-                                color="textSecondary"
-                                variant="body1"
-                            >
-                                or login with username
-                            </Typography>
-                        </Box>
-                        <TextField
-                            error={Boolean(formik.touched.username && formik.errors.username)}
-                            fullWidth
-                            helperText={formik.touched.username && formik.errors.username}
-                            label="Username"
-                            margin="normal"
-                            name="username"
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                            type="username"
-                            value={formik.values.username}
-                            variant="outlined"
-                        />
-                        <TextField
-                            error={Boolean(formik.touched.password && formik.errors.password)}
-                            fullWidth
-                            helperText={formik.touched.password && formik.errors.password}
-                            label="Password"
-                            margin="normal"
-                            name="password"
-                            onBlur={formik.handleBlur}
-                            onChange={formik.handleChange}
-                            type="password"
-                            value={formik.values.password}
-                            variant="outlined"
-                        />
-                        <Box sx={{py: 2}}>
-                            <Button
-                                color="primary"
-                                disabled={formik.isSubmitting}
-                                fullWidth
-                                size="large"
-                                type="submit"
-                                variant="contained"
-                            >
-                                Sign In Now
-                            </Button>
-                        </Box>
-                        <Typography
-                            color="textSecondary"
-                            variant="body2"
-                        >
-                            Don&apos;t have an account?
-                            {' '}
-                            <NextLink
-                                href="/register"
-                            >
-                                <Link
-                                    to="/register"
-                                    variant="subtitle2"
-                                    underline="hover"
-                                    sx={{
-                                        cursor: 'pointer'
-                                    }}
-                                >
-                                    Sign Up
-                                </Link>
-                            </NextLink>
-                        </Typography>
-                    </form>
-                </Container>
+  return (
+    <>
+      <Head>
+        <title>Login | Broker Demo</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: "center",
+          display: "flex",
+          flexGrow: 1,
+          minHeight: "100%",
+        }}
+      >
+        <Container maxWidth="sm">
+          <NextLink href="/" passHref>
+            <Button component="a" startIcon={<ArrowBackIcon fontSize="small" />}>
+              Dashboard
+            </Button>
+          </NextLink>
+          <form onSubmit={formik.handleSubmit}>
+            <Box sx={{ my: 3 }}>
+              <Typography color="textPrimary" variant="h4">
+                Sign in
+              </Typography>
+              <Typography color="textSecondary" gutterBottom variant="body2">
+                Sign in on the internal platform
+              </Typography>
             </Box>
-        </>
-    );
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <Button
+                  color="info"
+                  fullWidth
+                  startIcon={<FacebookIcon />}
+                  onClick={formik.handleSubmit}
+                  size="large"
+                  variant="contained"
+                >
+                  Login with Facebook
+                </Button>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Button
+                  fullWidth
+                  color="error"
+                  startIcon={<GoogleIcon />}
+                  onClick={formik.handleSubmit}
+                  size="large"
+                  variant="contained"
+                >
+                  Login with Google
+                </Button>
+              </Grid>
+            </Grid>
+            <Box
+              sx={{
+                pb: 1,
+                pt: 3,
+              }}
+            >
+              <Typography align="center" color="textSecondary" variant="body1">
+                or login with username
+              </Typography>
+            </Box>
+            <TextField
+              error={Boolean(formik.touched.username && formik.errors.username)}
+              fullWidth
+              helperText={formik.touched.username && formik.errors.username}
+              label="Username"
+              margin="normal"
+              name="username"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="username"
+              value={formik.values.username}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.password && formik.errors.password)}
+              fullWidth
+              helperText={formik.touched.password && formik.errors.password}
+              label="Password"
+              margin="normal"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="password"
+              value={formik.values.password}
+              variant="outlined"
+            />
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                disabled={formik.isSubmitting}
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Sign In Now
+              </Button>
+            </Box>
+            <Typography color="textSecondary" variant="body2">
+              Don&apos;t have an account?{" "}
+              <NextLink href="/register">
+                <Link
+                  to="/register"
+                  variant="subtitle2"
+                  underline="hover"
+                  sx={{
+                    cursor: "pointer",
+                  }}
+                >
+                  Sign Up
+                </Link>
+              </NextLink>
+            </Typography>
+          </form>
+        </Container>
+      </Box>
+    </>
+  );
 };
 
 export default Login;
